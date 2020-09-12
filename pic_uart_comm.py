c04a79757b7fb47030102d0f8945e536ab708f44
Danilo Lazic <lazicdanilo47@gmail.com>
2020-09-12 19:33:33 +0200

    Added funcionality

--- pic_uart_comm.py
+++ pic_uart_comm.py
@@ -14,12 +14,22 @@ stop_msg = 109  # 'm'
 start_msg = 77  # 'M'
 get_data_stop = 0
 
+vel_get_delay_s = 0.1
+get_vel_stop_flg = 0
 
 
 def int16_to_int(x):
     tmp1 = m.floor(x/256)
     tmp2 = x%256
     return tmp1, tmp2
+
+
+def get_vel():
+    while True:
+        if get_vel_stop_flg != 1:
+            pic_send(118)
+            
+        t.sleep(vel_get_delay_s)
 
 
 def get_data():
@@ -71,27 +81,39 @@ def init():
 
 
 def menu_and_choice():
-    global get_data_stop
+    global get_data_stop, get_vel_stop_flg
     print("\n\n1. START")
     print("2. STOP")
     print("3. PID set")
     print("4. Set speed")
+    print("5. Get speed")
 
     
    
     choice = int(input(">> "))
  
     if choice == 1:
+        get_vel_stop_flg = 1
+        t.sleep(0.1)
+
         pic_send(start_msg)
 
         speed = int(input("Input speed [0 - 6000]"))
         tmp1, tmp2 = int16_to_int(speed)
+
         pic_send(tmp1)
         pic_send(tmp2)
+        t.sleep(0.1)
+        get_vel_stop_flg = 0
+
 
  
     elif choice == 2:
+        get_vel_stop_flg = 1
+        t.sleep(0.1)
         pic_send(stop_msg)   # 'm'
+        t.sleep(0.1)
+        get_vel_stop_flg = 0
  
     elif choice == 3:
         get_data_stop = 1
@@ -99,37 +121,57 @@ def menu_and_choice():
         if what_pid == 1:
             new_kp = int(input("Input Kp\n")) 
             tmp1, tmp2 = int16_to_int(new_kp)
+            get_vel_stop_flg = 1
+            t.sleep(0.1)
             pic_send(112)
             pic_send(tmp1)
             pic_send(tmp2)
+            t.sleep(0.1)
+            get_vel_stop_flg = 0
 
         elif what_pid == 2:
             new_ki = int(input("Input Ki\n")) 
             tmp1, tmp2 = int16_to_int(new_ki)
+            get_vel_stop_flg = 1
+            t.sleep(0.1)
             pic_send(105)
             pic_send(tmp1)
             pic_send(tmp2)
+            t.sleep(0.1)
+            get_vel_stop_flg = 0
 
         elif what_pid == 3:
             new_kd = int(input("Input Kd\n")) 
             tmp1, tmp2 = int16_to_int(new_kd)
+            get_vel_stop_flg = 1
+            t.sleep(0.1)
             pic_send(100)
             pic_send(tmp1)
             pic_send(tmp2)
+            t.sleep(0.1)
+            get_vel_stop_flg = 0
+
         get_data_stop = 0
 
     elif choice == 4:
         pic_send(115)
         what_speed = int(input("What speed?"))
         tmp1, tmp2 = int16_to_int(what_speed)
+        get_vel_stop_flg = 1
+        t.sleep(0.1)
         pic_send(tmp1)
         pic_send(tmp2)
+        t.sleep(0.1)
+        get_vel_stop_flg = 0
 
 
 
 init()
 thread = threading.Thread(target=get_data)
 thread.start()
+
+thread_get_vel = threading.Thread(target=get_vel)
+thread_get_vel.start()
 
 while True:
     menu_and_choice()
